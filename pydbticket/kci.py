fa98ec8b26955020cf1676b85840595a5f2ea8fc
Simeon Keske <git@n0emis.eu>
2020-07-22 13:19:01 +0200

    cosmetical: make kci-body look nicer

--- pydbticket/kci.py
+++ pydbticket/kci.py
@@ -50,7 +50,8 @@ def checkin(ticket: Ticket, leg: Leg,
                 "bcb_erforderlich": "N",
                 "tkey": ticket.key,
                 "issuer": ticket.issuer,
-                "reisender_vorname": ticket.forename},
+                "reisender_vorname": ticket.forename,
+            },
             "zug": {
                 "nr": leg.number,
                 "gat": leg.kind,
@@ -73,12 +74,17 @@ def checkin(ticket: Ticket, leg: Leg,
                 "zeit": leg.arrival.datetime.isoformat() + 'Z',
                 "ebhf_name": leg.arrival.station_name,
                 "eva_name": leg.arrival.station_name,
-                "eva_nr": leg.arrival.station_number},
+                "eva_nr": leg.arrival.station_number,
+            },
             "bc_rabatts": [],
             "plaetze": [
                 {
                     "wagennr": int(coach),
-                    "platznr": int(seat)}]}}
+                    "platznr": int(seat),
+                },
+            ],
+        },
+    }
 
     request_body = json.dumps(body)
 
